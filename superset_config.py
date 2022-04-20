d27736411bb3a976cd85b1b4f21abf75432aff18
Evan Danaher <edanaher@admithub.com>
2022-04-20 09:32:33 -0400

    Various config updates from Bethany
    - Mainstay branding
    - RBAC feature flag
    - row limits

--- superset_config.py
+++ superset_config.py
@@ -9,6 +9,79 @@ from cachelib.redis import RedisCache
 
 logger = logging.getLogger()
 
+LOGO_TARGET_PATH = '/'
+APP_NAME = "Mainstay Analytics"
+
+FAVICONS = [{ "href":"https://d15jbbxs3h1fzs.cloudfront.net/favicon-16x16.png"}]
+
+APP_ICON = "https://d15jbbxs3h1fzs.cloudfront.net/mainstay-logo-horiz.png"
+
+EXTRA_CATEGORICAL_COLOR_SCHEMES = [
+     {
+        "id": 'mainstayDark',
+        "description": '',
+         "label": 'Mainstay Dark',
+        "isDefault": False,
+        "colors":
+    ['#092E63',
+      '#DF2C55',
+      '#039855',
+      '#FEC84B',
+      '#1D2939',
+      '#CB271B',
+      '#FCC700',
+      '#620B3E',
+      '#00FFD4',
+      '#00E0FF']
+
+    },
+    {
+        "id": 'mainstayPastel',
+        "description": '',
+         "label": 'Mainstay Pastel',
+        "isDefault": False,
+        "colors":
+      ['#FCCDD7',
+      '#E7EBF0',
+      '#D1FADF',
+      '#FFF0C8',
+      '#D0D5DD',
+      '#FEE4E2',
+      '#FFFCF5',
+      '#F4A0D2',
+      '#CCFFF6',
+      '#BFF7FF',
+    ]
+    },
+         {
+        "id": 'mainstayFull',
+        "description": '',
+         "label": 'Mainstay Fullcolor',
+        "isDefault": True,
+        "colors":
+      ['#092E63',
+      '#DF2C55',
+      '#039855',
+      '#FEC84B',
+      '#1D2939',
+      '#CB271B',
+      '#FCC700',
+      '#620B3E',
+      '#00FFD4',
+      '#00E0FF',
+      '#FCCDD7',
+      '#E7EBF0',
+      '#D1FADF',
+      '#FFF0C8',
+      '#D0D5DD',
+      '#FEE4E2',
+      '#FFFCF5',
+      '#F4A0D2',
+      '#CCFFF6',
+      '#BFF7FF',
+    ]
+    }]
+
 
 def get_env_variable(var_name: str, default: Optional[str] = None) -> str:
     """Get the environment variable or raise exception."""
@@ -23,7 +96,14 @@ def get_env_variable(var_name: str, default: Optional[str] = None) -> str:
             )
             raise EnvironmentError(error_msg)
 
+## Data Amounts
 
+# default row limit when requesting chart data
+ROW_LIMIT = 1000000
+# default row limit when requesting samples from datasource in explore view
+SAMPLES_ROW_LIMIT = 150
+# max rows retrieved by filter select auto complete
+FILTER_SELECT_ROW_LIMIT = 10000
 DATABASE_DIALECT = get_env_variable("DATABASE_DIALECT")
 DATABASE_USER = get_env_variable("DATABASE_USER")
 DATABASE_PASSWORD = get_env_variable("DATABASE_PASSWORD")
@@ -74,6 +154,9 @@ CELERY_CONFIG = CeleryConfig
 RESULTS_BACKEND = RedisCache(
     host=REDIS_HOST, port=REDIS_PORT, key_prefix='superset_results')
 
+FEATURE_FLAGS = {"ALERT_REPORTS": True,
+                 "DASHBOARD_RBAC": True}
+
 # TODO: Should we add the other caches?
 FILTER_STATE_CACHE_CONFIG = {
     "CACHE_TYPE": "redis",
@@ -88,7 +171,6 @@ EXPLORE_FORM_DATA_CACHE_CONFIG = {
     "CACHE_REDIS_DB": REDIS_CACHE_DB,
 }
 
-FEATURE_FLAGS = {"ALERT_REPORTS": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
